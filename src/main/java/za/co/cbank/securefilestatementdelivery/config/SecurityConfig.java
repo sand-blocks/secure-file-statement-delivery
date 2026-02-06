d5ce41e88c27e82f1b4d49c989cb4df9397e8917
Charlin Martin <charlinmartin@gmail.com>
2026-02-06 05:02:30 +0200

    remove security for healthcheck

--- src/main/java/za/co/cbank/securefilestatementdelivery/config/SecurityConfig.java
+++ src/main/java/za/co/cbank/securefilestatementdelivery/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
 				.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/api/v1/public/**").permitAll()
+						.requestMatchers("/actuator/**").permitAll()
 						.anyRequest().authenticated()
 				)
 				.httpBasic(Customizer.withDefaults());
